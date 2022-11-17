747cd474af5bbb45049487a67637881a45e8f78e
Ksenya Nemkina <xenya.nemkina@yandex.ru>
2022-11-17 19:36:37 +0300

    feat: создание страницы

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__close");
+const popupOpenButtonElement = document.querySelector(".info__button");
+const popupSaveButtonElement = document.querySelector(".popup__save");
+
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__field_name");
+let jobInput = formElement.querySelector(".popup__field_prof");
+let infoName = document.querySelector(".info__name");
+let infoProfession = document.querySelector(".info__profession");
+
+const openPopup = function () {
+  popupElement.classList.add("popup_is-opened");
+  nameInput.value;
+  jobInput.value;
+};
+const closePopup = function () {
+  popupElement.classList.remove("popup_is-opened");
+};
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  infoName.textContent = nameInput.value;
+  infoProfession.textContent = jobInput.value;
+}
+formElement.addEventListener("submit", formSubmitHandler);
+
+popupOpenButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+popupSaveButtonElement.addEventListener("click", closePopup);
